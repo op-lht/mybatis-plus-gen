1724533f9dacf3fa9bd6e5b2c15aff79cc629008
lihuatian <op_zui@163.com>
2018-12-27 16:14:21 +0800

    强迫症犯了

--- src/main/java/com/lht/Application.java
+++ src/main/java/com/lht/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MybatisPlus1Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MybatisPlus1Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
